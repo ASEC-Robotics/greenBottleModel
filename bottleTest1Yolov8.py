dccc5d75acfc14e31977aa5dcd933a68b9b54a04
Sina Glamocic <sglamocic@gmail.com>
2023-02-24 21:20:39 -0800

    Add files via upload
    trained model and python script for testing

---
+++ bottleTest1Yolov8.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import cv2
+
+model = YOLO("/Users/sina/Desktop/bottleTest/best.pt")#edit path according to your setup
+model.predict(source="0", show=True, conf=0.5)
