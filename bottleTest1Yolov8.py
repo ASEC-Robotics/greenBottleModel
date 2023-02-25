06d7469a5e88060d667cafdc36b70fbd7b4b9de3
Sina Glamocic <sglamocic@gmail.com>
2023-02-24 21:26:12 -0800

    Update bottleTest1Yolov8.py
    path changed

--- bottleTest1Yolov8.py
+++ bottleTest1Yolov8.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
-model = YOLO("/Users/sina/Desktop/bottleTest/best.pt")#edit path according to your setup
+model = YOLO("/Users/.../Desktop/bottleTest/best.pt")#edit path according to your setup
 model.predict(source="0", show=True, conf=0.5)
